7246dfeb5aea6920e1a6076a17b89215bd011119
Bruna Castelo <bruno.castelo@sonicdrivein.com>
2020-11-13 09:09:01 -0300

    first commit

---
+++ react-dice/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  const firstDieImage = require(`./assets/1.png`);
+  const secondDieImage = require(`./assets/6.png`);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div style={{ display: 'flex', margin: 20 }}>
+          <img src={firstDieImage} className="die" alt="Die one" />
+          <img src={secondDieImage} className="die" alt="Die two" />
+        </div>
+        <span>{1 + 6}</span>
+        <button className="button" >Roll</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
